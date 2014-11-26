e16eea00e57fe394b2ffde3e98d1f134f854a691
Happy <iamchuzhiyan@gmail.com>
2014-11-26 21:13:35 +0800

    Submit exec1.c

---
+++ C Primer Plus/第11章 字符串和字符串函数/编程练习/exec1/exec1.c
@@ -0,0 +1,50 @@
+﻿#include <stdio.h>
+#define N 10
+//函数声明
+//从输入读取n个字符（包括空格、制表符和换行符），把结果存储在一个数组中
+char *getnchar(char *str, int n);
+
+//主函数
+int main(void)
+{
+	char input[N];
+	char *chk;
+	chk = getnchar(input, N-1);
+	if (NULL == chk)
+	{
+		puts("Input failed.");
+	} 
+	else
+	{
+		puts(input);
+	}
+	puts("Done. \n");
+	return 0;
+}
+
+char *getnchar(char *str, int n)
+{
+	int i; 
+	int ch;
+	for (i=0; i<n; ++i)
+	{
+		ch = getchar();
+		if (ch != EOF)
+		{
+			str[i] = ch;
+		}
+		else
+		{
+			break;
+		}
+	}
+	if (ch == EOF)
+	{
+		return NULL;
+	} 
+	else
+	{
+		str[i] = '\0';
+		return str;
+	}
+}

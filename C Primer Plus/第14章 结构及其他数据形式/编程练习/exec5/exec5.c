1c0754131be9599de595b0454df6c870969d2fba
Happy <iamchuzhiyan@gmail.com>
2014-12-04 21:45:43 +0800

    Update exec5.c

--- C Primer Plus/第14章 结构及其他数据形式/编程练习/exec5/exec5.c
+++ C Primer Plus/第14章 结构及其他数据形式/编程练习/exec5/exec5.c
@@ -1,6 +1,8 @@
 //  [12/4/2014 Happy]
+#include <stdio.h>
 #define MAX 10
 #define CSIZE 4
+#define LEN 3
 typedef struct names {
 	char first[MAX];
 	char last[MAX];
@@ -9,8 +11,12 @@ typedef struct names {
 typedef struct students {
 	Name name;
 	double grade[3];
+	double average;
 } Students;
 
+void input(Students s[], int len);
+double calculateAverage(Students s[], int len);
+void print(Students s[], int len);
 int main(void)
 {
 	//声明一个长度为4的Students的数组，随意初始化名字部分
@@ -28,5 +34,82 @@ int main(void)
 			{"Bob","Green"}
 		},
 	};
+	input(s,CSIZE);
+	calculateAverage(s,CSIZE);
+	print(s,CSIZE);
 	return 0;
+}
+
+/*
+ * @function    : input
+ * @author   	: ZhangLe
+ * @date     	: 2014/12/4 21:37
+ * @version  	: ver 1.0
+ * @inparam  	: void
+ * @outparam    : void
+ * @description : 输入数据
+ */
+void input(Students s[], int len)
+{
+	int i;	//循环标记
+	puts("Please enter data for students.");
+	for (i=0; i<len; ++i)
+	{
+		puts("Please enter name and grade.");
+		scanf("%s %s %.2f %.2f %.2f",&s[i].name.first, &s[i].name.last, &s[i].grade[0], &s[i].grade[1], &s[i].grade[2]);
+	}
+}
+/*
+ * @function    : calculateAverage
+ * @author   	: ZhangLe
+ * @date     	: 2014/12/4 21:37
+ * @version  	: ver 1.0
+ * @inparam  	: void
+ * @outparam    : void
+ * @description : 计算平均分
+ */
+double calculateAverage(Students s[], int len)
+{
+	int i, j;	//循环标记
+	double total;	//每个人的总分
+	double avg;
+	puts("Claculate Average...");
+	for (i=0; i<len; ++i)
+	{
+		//求出总分
+		for (j=0; j<LEN; ++j)	
+		{
+			total += s[i].grade[i];
+		}
+		//求出平均分
+		avg = total / 3;
+		//把平均分赋值给对应的人
+		s[i].average = avg;
+	}
+}
+/*
+ * @function    : print
+ * @author   	: ZhangLe
+ * @date     	: 2014/12/4 21:41
+ * @version  	: ver 1.0
+ * @inparam  	: void
+ * @outparam    : void
+ * @description : 输出每个结构的信息
+ */
+void print(Students s[], int len)
+{
+	int i, j;	//循环标记
+	puts("Output info...");
+	for (i=0; i<len; ++i)
+	{
+		//输出姓名
+		printf("%s %s",s[i].name.last,s[i].name.first);
+		//输出分数
+		for (j=0; j<LEN; ++j)	
+		{
+			printf("%.2f",s[i].grade[i]);
+		}
+		//输出平均分
+		printf("%.2f",s[i].average);
+	}
 }
